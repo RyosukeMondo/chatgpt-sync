b3ec8e02bf572bcc8b1bf9ee8b3cc2311ec5ad1e
Ryosuke Mondo <ryosuke.mondo@gmail.com>
2024-10-26 21:44:46 +0900

    Add .nvmrc, .npmrc, .eslintignore, .gitignore, .example.env, .husky/pre-commit, .prettierignore, pnpm-workspace.yaml, vite-env.d.ts, .prettierrc, vite-config, and tsconfig files

---
+++ pages/content-ui/src/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from 'react';
+import { Button } from '@extension/ui';
+import { useStorage } from '@extension/shared';
+import { exampleThemeStorage } from '@extension/storage';
+
+export default function App() {
+  const theme = useStorage(exampleThemeStorage);
+
+  useEffect(() => {
+    console.log('content ui loaded');
+    // get body and console.log it
+    console.log(document.body);
+  }, []);
+
+  return (
+    <div className="flex items-center justify-between gap-2 rounded bg-blue-100 px-2 py-1">
+      <div className="flex gap-1 text-blue-500">
+        Edit <strong className="text-blue-700">pages/content-ui/src/app.tsx</strong> and save to reload2.
+      </div>
+      <Button theme={theme} onClick={exampleThemeStorage.toggle}>
+        Toggle Theme
+      </Button>
+    </div>
+  );
+}
---
+++ pages/content/src/index.ts
@@ -0,0 +1,6 @@
+import { toggleTheme } from '@src/toggleTheme';
+
+console.log('content script loaded2');
+console.log(document.body);
+
+void toggleTheme();
